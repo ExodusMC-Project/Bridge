e84924d5bb5d58f6f86e815637e6cd463ae31c68
Hasan Demirtaş <utsukushihito@outlook.com>
2023-02-17 16:12:07 +0300

    Update Pipeline.java

--- src/main/java/net/pixteria/bridge/future/Pipeline.java
+++ src/main/java/net/pixteria/bridge/future/Pipeline.java
@@ -25,7 +25,7 @@ public final class Pipeline {
 
     public <T extends Event> void register(final String topic, final Class<T> cls, final Consumer<T> consumer) {
         this.topics.computeIfAbsent(topic, redis::getTopic).addListener(cls, (channel, msg) -> consumer.accept(msg));
-        if (cls.isAssignableFrom(EventResponsible.class)) {
+        if (EventResponsible.class.isAssignableFrom(cls)) {
             this.topics.computeIfAbsent(topic, redis::getTopic).addListener(Response.class, (channel, msg) -> {
                 final var future = this.responses.get(msg.request());
                 if (future != null) {
